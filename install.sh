8a4e73aed4cb56f13638e24ea98ed305bff44556
fuzifuziii <mops.sops123@gmail.com>
2026-07-20 23:49:07 +0300

    waybar settings text

--- install.sh
+++ install.sh
@@ -228,7 +228,7 @@ s_menu() {
   echo -e "4) Install CUPS"
   echo -e "5) Install Xone drivers"
   echo -e "6) Exit"
-  echo -e "${BLUE}=================V2.1==================${NC}"
+  echo -e "${BLUE}=================V2.2==================${NC}"
 }
 
 while true; do
